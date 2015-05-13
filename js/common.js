572aa1d040407ea8b5636193a3495769a98e3529
maximBegar <maxbeg091@mail.ru>
2015-05-13 22:53:33 +0300

    boyan

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(function()
+{
+  $('.question_Block').click(function()
+  {
+     $('.question_Block').not(this).removeClass('opened');
+     $(this).toggleClass('opened').next('.answer_').slideToggle(400);
+     $('.answer_').not($(this).next('.answer_')).slideUp(400);
+     $('.arrow').not($(this).find('.arrow')).prop('src','img/arrow-bottom.png');
+
+     if($(this).hasClass('opened')){
+        $(this).find('.arrow').prop('src','img/arrow-top.png');
+     }
+     else{
+        $(this).find('.arrow').prop('src','img/arrow-bottom.png');
+     }
+  });
+});
