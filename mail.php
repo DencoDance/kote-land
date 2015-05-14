a28f64cd694c31e8db580fc6d8a93b4035edd051
DencoDance <di.nekto@gmail.com>
2015-05-14 15:01:42 +0300

    mail

---
+++ mail.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DencoDance
+ * Date: 14.05.2015
+ * Time: 12:52
+ */
+function complete_mail() {
+    // $_POST['title'] содержит данные из поля "Тема", trim() - убираем все лишние пробелы и переносы строк, htmlspecialchars() - преобразует специальные символы в HTML сущности, будем считать для того, чтобы простейшие попытки взломать наш сайт обломались, ну и  substr($_POST['title'], 0, 1000) - урезаем текст до 1000 символов. Для переменных $_POST['mess'], $_POST['name'], $_POST['tel'], $_POST['email'] все аналогично
+    $_POST['clientName'] =  substr(htmlspecialchars(trim($_POST['clientName'])), 0, 30);
+    $_POST['clientPhone'] =  substr(htmlspecialchars(trim($_POST['clientPhone'])), 0, 30);
+//    var_dump($_POST);
+    // если не заполнено поле "Имя" - показываем ошибку 0
+    if (empty($_POST['clientName']))
+        output_err(0);
+    // если не заполнено поле "Сообщение" - показываем ошибку 2
+    if(empty($_POST['clientPhone']))
+        output_err(1);
+    // создаем наше сообщение
+    $mess = '<html>
+                <title>Новая "жертва" лендинга bustogeorgia</title>'.
+            '<body>
+                     <br/>'.'Имя отправителя:'.$_POST['clientName'].
+                    '<br/>'.'Контактный телефон:'.$_POST['clientPhone'].
+            '</body>
+             </html>';
+    $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+    // $to - кому отправляем
+    $to = 'di.nekto@gmail.com';
+    // $from - от кого
+    $from='bustogorgia landing';
+    mail($to, 'New contacts from landing', $mess
+        , "Content-type: text/html; charset=windows-1251 \r\n From:".$from
+    );
+    echo 'Спасибо! Ваше письмо отправлено.';
+    header('Location: http://bustogeorgia.in.ua');
+}
+
+function output_err($num)
+{
+    $err[0] = 'ОШИБКА! Не введено имя.';
+    $err[1] = 'ОШИБКА! Неверно введен мобильный.';
+    echo '<p>'.$err[$num].'</p>';
+//    show_form();
+    exit();
+}
+
+if (!empty($_POST['clientName']) && !empty($_POST['clientPhone']) ){ complete_mail();var_dump('sended');}
+else header('Location: http://bustogeorgia.in.ua');
+
+?>
